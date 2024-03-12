fcef3700dfe1a6c09cac3b5a550aa985fe518090
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-11 23:02:19 -0400

    Add colors
    Signed-off-by: MSCompSci <62676614+MSCompSci@users.noreply.github.com>

--- website/client/tailwind.config.cjs
+++ website/client/tailwind.config.cjs
@@ -1,28 +1,46 @@
 /** @type {import('tailwindcss').Config}*/
+const colors = require("tailwindcss/colors");
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
 
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      stone: colors.stone,
+      sky: colors.sky,
+      neutral: colors.neutral,
+      gray: colors.gray,
+      slate: colors.slate,
+    },
     extend: {
       colors: {
         primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B'
-        }
-      }
+          50: "#FFF5F2",
+          100: "#FFF1EE",
+          200: "#FFE4DE",
+          300: "#FFD5CC",
+          400: "#FFBCAD",
+          500: "#FE795D",
+          600: "#EF562F",
+          700: "#EB4F27",
+          800: "#CC4522",
+          900: "#A5371B",
+        },
+      },
     },
   },
 
-  plugins: [require('flowbite/plugin')],
-  darkMode: 'class'
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
 };
 
 module.exports = config;
