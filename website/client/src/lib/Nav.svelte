0c5bca955e2d37441fc1e272f3f834d8e4a68b43
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-11 23:03:06 -0400

    Remove template items, add nav
    Signed-off-by: MSCompSci <62676614+MSCompSci@users.noreply.github.com>

---
+++ website/client/src/lib/Nav.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {
+    Navbar,
+    NavBrand,
+    NavUl,
+    NavHamburger,
+    DarkMode,
+    NavLi,
+  } from "flowbite-svelte";
+</script>
+
+<Navbar rounded color="form" class="w-5/6 mx-auto">
+  <NavBrand href="/">
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+      >Logo</span
+    >
+  </NavBrand>
+  <ul class="flex flex-row items-center gap-8">
+    <li>Location</li>
+    <li><DarkMode /></li>
+  </ul>
+</Navbar>
