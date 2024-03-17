549d4897636111fcd931c83aab8fcedaa4457f3e
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-17 19:55:34 -0400

    Create banner

---
+++ website/client/src/lib/Banner.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { Banner } from 'flowbite-svelte';
+    import { Alert } from 'flowbite-svelte';
+    import "@fortawesome/fontawesome-free/css/all.min.css";
+    export let shortMessage:string = "Test Message"
+    export let fullMessage:string = "Test full message"
+    let dialog:HTMLDialogElement;
+    let open = false;
+    function dialogToggle(){
+        if(!open){
+            dialog.show();
+            open=true;
+        }
+        else {
+            dialog.close()
+            open=false;
+        }
+    }
+</script>
+<Banner id="banner" divClass="z-10 flex justify-between p-4 dark:bg-slate-700 dark:border-slate-600" dismissable={false}>
+    <p class="flex items-center text-sm font-normal text-slate-500 dark:text-slate-400">
+      <span>
+        <button type="button" on:click={dialogToggle}>
+            {shortMessage}
+            <span class="sr-only">View full message</span>
+            <i class:fa-chevron-up={open} class:fa-chevron-down={!open} class="fa-solid"></i>
+        </button>
+      </span>
+    </p>
+</Banner>
+<dialog bind:this={dialog} class="relative w-full text-center py-8">
+    <h2>{fullMessage}</h2>
+</dialog>
