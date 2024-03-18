f79d8cc2a63d3c24e7f54ede3c388ca5edec638b
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-17 20:33:42 -0400

    Add current condition component

---
+++ website/client/src/lib/CurrentCondition.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let condition:string;
+</script>
+<div class="w-56 h-56 bg-slate-50 border flex flex-col gap-2 items-center justify-center">
+    <h3 class="sr-only">Current Condition</h3>
+    {#if condition === "Sunny" || condition==="Clear" || condition==="Mostly Clear" || condition==="Mostly Sunny"}
+    <i class="fa-regular fa-sun fa-6x"></i>
+    {:else if condition==="Partly Cloudy" || condition==="Partly Sunny"}
+    <i class="fa-solid fa-cloud-sun fa-6x"></i>
+    {:else if condition==="Mostly Cloudy" || condition==="Cloudy"}
+    <i class="fa-solid fa-cloud-sun fa-6x"></i>
+    {:else if condition==="Scattered Showers"}
+    <i class="fa-solid fa-cloud-sun-rain fa-6x"></i>
+    {:else if condition==="Rain"}
+    <i class="fa-solid fa-cloud-rain fa-6x"></i>
+    {:else if condition==="Heavy Showers"}
+    <i class="fa-solid fa-cloud-showers-heavy fa-6x"></i>
+    {:else if condition==="Thunderstorm"}
+    <i class="fa-solid fa-cloud-bolt fa-6x"></i>
+    {:else if condition==="Snow"}
+    <i class="fa-solid fa-snowflake fa-6x"></i>
+    {:else if condition==="Windy"}
+    <i class="fa-solid fa-wind fa-6x"></i>
+    {/if}
+    <p class="text-2xl">{condition}</p>
+</div>
