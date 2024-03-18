435d1afbc44e853b22a4377250b14d540d6f38d8
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-17 20:34:00 -0400

    Add card

---
+++ website/client/src/lib/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let title:string|null = null;
+    export let position:string;
+</script>
+<section class={position+" bg-slate-100 border p-8 rounded"}>
+    {#if title}
+    <h2 class="text-2xl font-semibold ">{title}</h2>
+    {/if}
+    <slot name="content"></slot>
+</section>
