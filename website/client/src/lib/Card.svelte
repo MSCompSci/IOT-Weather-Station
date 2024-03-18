3a71fc076f256ef79751894ca38fd466e2050637
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-17 22:43:04 -0400

    Update card styling

--- website/client/src/lib/Card.svelte
+++ website/client/src/lib/Card.svelte
@@ -2,9 +2,9 @@
     export let title:string|null = null;
     export let position:string;
 </script>
-<section class={position+" bg-slate-100 border p-8 rounded"}>
+<section class={position+" bg-slate-200 border p-8 rounded"}>
     {#if title}
-    <h2 class="text-2xl font-semibold ">{title}</h2>
+    <h2 class="text-4xl font-semibold ">{title}</h2>
     {/if}
     <slot name="content"></slot>
 </section>
