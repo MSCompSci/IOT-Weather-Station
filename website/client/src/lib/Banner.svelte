214d1df6cb8adebf24fe9d51a25a1317a4978cc3
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-17 20:07:25 -0400

    Style banner

--- website/client/src/lib/Banner.svelte
+++ website/client/src/lib/Banner.svelte
@@ -18,16 +18,16 @@
     }
 </script>
 <Banner id="banner" divClass="z-10 flex justify-between p-4 dark:bg-slate-700 dark:border-slate-600" dismissable={false}>
-    <p class="flex items-center text-sm font-normal text-slate-500 dark:text-slate-400">
+    <p class="flex items-center font-normal text-slate-500 dark:text-slate-200 text-base">
       <span>
         <button type="button" on:click={dialogToggle}>
             {shortMessage}
             <span class="sr-only">View full message</span>
-            <i class:fa-chevron-up={open} class:fa-chevron-down={!open} class="fa-solid"></i>
+            <i class:fa-chevron-up={open} class:fa-chevron-down={!open} class="fa-solid fa-xl"></i>
         </button>
       </span>
     </p>
 </Banner>
-<dialog bind:this={dialog} class="relative w-full text-center py-8">
+<dialog bind:this={dialog} class="relative w-full text-center py-8 bg-slate-100 dark:bg-slate-700 dark:text-slate-200 border-b dark:border-slate-600">
     <h2>{fullMessage}</h2>
 </dialog>
