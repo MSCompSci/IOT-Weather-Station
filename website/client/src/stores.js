2a1573882ed08df82bf8048ed724fc5735bb29ab
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-28 18:34:07 -0400

    Add public test data and units store

--- website/client/src/stores.js
+++ website/client/src/stores.js
@@ -1,8 +1,11 @@
-import {readable} from 'svelte/store';
+import {readable, writable} from 'svelte/store';
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
+
+// Site settings
+let units = writable("Imperial");
 
 // Readings from raspberry pi
 let temp = readable(0,(set)=>{
@@ -50,5 +53,51 @@ let airPressure = readable("25.90",(set)=>{
         clearInterval(incrementCounter)
     }
 })
+// Public readings
+let pubTemp = readable(0,(set)=>{
+    let incrementCounter = setInterval(()=>{
+        let newVal = randomIntFromInterval(-100,150)
+        set (newVal);
+    }, 1000)
+    return ()=>{
+        clearInterval(incrementCounter)
+    }
+})
+let pubAirQuality = readable(0,(set)=>{
+    let incrementCounter = setInterval(()=>{
+        let newVal = randomIntFromInterval(0,100)
+        set (newVal);
+    }, 1000)
+    return ()=>{
+        clearInterval(incrementCounter)
+    }
+})
+let pubLight = readable(0,(set)=>{
+    let incrementCounter = setInterval(()=>{
+        let newVal = randomIntFromInterval(0,10)
+        set (newVal);
+    }, 1000)
+    return ()=>{
+        clearInterval(incrementCounter)
+    }
+})
+let pubHumidity = readable(0,(set)=>{
+    let incrementCounter = setInterval(()=>{
+        let newVal = randomIntFromInterval(0,100)
+        set (newVal);
+    }, 1000)
+    return ()=>{
+        clearInterval(incrementCounter)
+    }
+})
+let pubAirPressure = readable("25.90",(set)=>{
+    let incrementCounter = setInterval(()=>{
+        let newVal = Math.floor(Math.random() * (32.01 - 25.90 + 1.00) + 25.9).toFixed(2)
+        set (newVal);
+    }, 1000)
+    return ()=>{
+        clearInterval(incrementCounter)
+    }
+})
 
-export {temp, airQuality, light, humidity, airPressure}
+export {units, temp, airQuality, light, humidity, airPressure, pubTemp, pubLight, pubHumidity, pubAirQuality, pubAirPressure}
