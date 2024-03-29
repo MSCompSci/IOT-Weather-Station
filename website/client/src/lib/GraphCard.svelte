0d2c5efae4cf8a54e4dd5bc98c8020af3d343565
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-28 23:03:02 -0400

    Add basic graph

--- website/client/src/lib/GraphCard.svelte
+++ website/client/src/lib/GraphCard.svelte
@@ -2,23 +2,28 @@
     export let position:string;
     import Toggle from "./Toggle.svelte";
     import { Tabs, TabItem } from 'flowbite-svelte';
+    import Graph from "./Graph.svelte";
 </script>
 <section class={position+" backdrop-blur-sm bg-gradient-to-br from-blue-300/30 to-blue-600/30 border border-blue-900/25 text-slate-50 font-coda p-8 rounded-lg "}>
 
-    <Tabs style="pill" class="flex flex-row items-center  justify-between text-sm font-medium text-center disabled:cursor-not-allowed">
+    <Tabs style="pill" contentClass="'p-0 bg-gradient-to-br from-blue-300/30 to-blue-600/30 rounded-lg mt-4" class="flex flex-row items-center  justify-between text-sm font-medium text-center disabled:cursor-not-allowed">
         <div class="flex flex-row gap-2 bg-gradient-to-br from-blue-300/30 to-blue-600/30 p-2 backdrop-blur-sm rounded-lg">
             <TabItem 
+            disabled
             defaultClass="inline-block  font-medium text-center disabled:cursor-not-allowed"
             inactiveClasses="px-4 py-2 rounded-lg hover:text-gray-600 hover:bg-gray-50 bg-gradient-to-br from-blue-50/30 to-blue-200/30 "
             activeClasses="px-4 py-2 rounded-lg hover:bg-gray-50 bg-gradient-to-br hover:from-blue-500/60 hover:to-blue-700/60 from-blue-700/60 to-blue-900/60 border-blue-900/25">
                 <span slot="title" class="text-xl">Forecast</span>
+                
             </TabItem>
             <TabItem 
             open
-            defaultClass="inline-block  font-medium text-center disabled:cursor-not-allowed"
+            
+            defaultClass="inline-block font-medium text-center disabled:cursor-not-allowed"
             inactiveClasses="px-4 py-2 rounded-lg hover:text-gray-600 hover:bg-gray-50 bg-gradient-to-br from-blue-50/30 to-blue-200/30 "
             activeClasses="px-4 py-2 rounded-lg hover:bg-gray-50 bg-gradient-to-br hover:from-blue-500/60 hover:to-blue-700/60 from-blue-700/60 to-blue-900/60 border-blue-900/25">
                 <span slot="title" class="text-xl">Past Data</span>
+                <Graph/>
             </TabItem>
         </div>
         <menu class="flex flex-row gap-2 justify-end ">
